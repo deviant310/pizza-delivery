aff326bdf186cdfd716cd22f422b665de6ab4433
Антон <antoinedzejob@gmail.com>
2020-05-30 13:52:50 +0300

    use dir fix

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const Express = require('express');
 const Path = require('path');
+const Minimist = require('minimist')
 
 const App = Express();
 
-const UseDir = './dist';
+const env = Minimist(process.argv.slice(2));
 
-App.use(Express.static(UseDir));
-App.use('/', (request, response) => {
-  response.sendFile(Path.resolve(UseDir, 'index.html'));
-});
+if(env['use-dir']){
+  App.use(Express.static(env['use-dir']));
+  App.use('/', (request, response) => {
+    response.sendFile(Path.resolve(env['use-dir'], 'index.html'));
+  });
 
-App.listen(8000);
+  App.listen(3000);
+} else {
+  throw new Error('--use-dir is not defined!')
+}
